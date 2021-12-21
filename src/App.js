f1c6465c7110706772d96080a4468b02b92c6aa1
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-12-20 20:18:10 -0800

    CollectionCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
-  return <div className="app">
-    <Header />
-  </div>
+  return (
+    <div className="app">
+      <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=s0"
+      />
+    </div>
+  );
 }
 
 export default App;
